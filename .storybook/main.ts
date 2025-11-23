d4e1f03f2d72ca9cd4472bf07a2d6d594ec6d7bd
SingUp_009 <009singup@gmail.com>
2025-11-23 22:36:22 +0900

    ➕ feat(ui): add storybook addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,13 @@ import type { StorybookConfig } from "@storybook/nextjs-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: ["@storybook/addon-interactions", "@storybook/addon-vitest"],
+  addons: [
+    "@storybook/addon-vitest",
+    "@storybook/addon-docs",
+    "@storybook/addon-a11y",
+    "@storybook/addon-links",
+    "@storybook/addon-vitest",
+  ],
   framework: {
     name: "@storybook/nextjs-vite",
     options: {},
